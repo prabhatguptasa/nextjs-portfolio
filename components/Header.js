c3474206bbdce8d4b5ead640541fc0466d8fd2d4
Prabhat Gupta <prabhug22@gmail.com>
2022-12-30 19:47:13 +0530

    updated skills

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-[url('https://media.licdn.com/dms/image/C5603AQFXD_9RS2ioRw/profile-displayphoto-shrink_200_200/0/1660982756930?e=1677715200&v=beta&t=2rYW-_eHLhuJwl4rX3jcEKKxHyMitipSqVtjLQkszCs')]" />
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
