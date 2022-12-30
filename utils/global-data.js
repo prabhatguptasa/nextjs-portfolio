31f294a2bfee4fc4b708da1b6fc9eba46e4db2cb
Prabhat Gupta <prabhug22@gmail.com>
2022-12-30 20:16:25 +0530

    updated

--- utils/global-data.js
+++ utils/global-data.js
@@ -7,7 +7,7 @@ export const getGlobalData = () => {
     : 'Software Engineer @ Gameopedia AS⚡ Rapid Development⚡ Quick Learner⚡End-User Focus';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.  Current Status: [![Netlify Status](https://api.netlify.com/api/v1/badges/9be1f6b8-7222-4177-b841-1c9099c1f1f1/deploy-status)](https://app.netlify.com/sites/chimerical-snickerdoodle-a36a00/deploys)';
+    : 'All rights reserved.';
 
   return {
     name,
