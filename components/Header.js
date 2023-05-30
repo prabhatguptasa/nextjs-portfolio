71dae1e748c4efa0b60e555d02dc70f3785022b7
Prabhat Gupta <prabhug22@gmail.com>
2023-05-30 16:16:00 +0530

    updated
    As per the linkedin profile

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="w-32 h-32 rounded-full block mx-auto mb-4 bg-cover bg-center bg-[url('https://media.licdn.com/dms/image/C5603AQFXD_9RS2ioRw/profile-displayphoto-shrink_200_200/0/1660982756930?e=1684368000&v=beta&t=TFyH0M2Ai9WxIRot98o6CZCztjHa99Q6p2mthle5m7s')]" />
+      <div className="w-32 h-32 rounded-full block mx-auto mb-4 bg-cover bg-center bg-[url('https://i.ibb.co/Ttm1NwD/20220820-132918.jpg')]" />
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
